a503bc9b5488faf45571f1c73c9e6480c4aca747
sxsandro1961-rgb <sxsandro1961@tutamail.com>
2026-08-08 11:32:58 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
